a35fd95f498371d5e7336167e03a8e3a0a1bdd39
Dan Reist <djreist@outlook.com>
2021-04-13 14:28:32 -0400

    Determine if Visual Studio is installed

--- .github/scripts/build.ps1
+++ .github/scripts/build.ps1
@@ -1,1 +1,6 @@
-Get-Variable
+# Set the verbose preference
+$VerbosePreference = 'Continue'
+
+# Locate vswhere.exe
+$vsWherePath = Join-Path -Path ( Join-Path -Path ( Join-Path -Path ${env:ProgramFiles(x86)} -ChildPath 'Microsoft Visual Studio' ) -ChildPath Installer ) -ChildPath vswhere.exe
+Write-Verbose -Message "vswhere.exe path: $vsWherePath"
