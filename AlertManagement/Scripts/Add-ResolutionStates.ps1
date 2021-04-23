64c27cfd35abe44f3c753b7961e6f05c9acbe87e
Dan Reist <djreist@outlook.com>
2021-04-23 08:12:34 -0400

    Added logging

--- AlertManagement/Scripts/Add-ResolutionStates.ps1
+++ AlertManagement/Scripts/Add-ResolutionStates.ps1
@@ -1,14 +1,74 @@
 ﻿[CmdletBinding()]
 param
-()
+(
+    [Parameter()]
+    [System.String]
+    $DebugLogging = 'false'
+)
+
+# Gather the start time of the script
+$startTime = Get-Date
+
+$debug = [System.Boolean]::Parse($DebugLogging)
+$parameterString = $PSBoundParameters.GetEnumerator() | ForEach-Object -Process { "`n$($_.Key) => $($_.Value)" }
+
+# Enable Write-Debug without inquiry when debug is enabled
+if ( $debug -or $DebugPreference -ne 'SilentlyContinue' )
+{
+    $DebugPreference = 'Continue'
+}
+
+$scriptName = 'Alert-SCOMAlert.ps1'
+$scriptEventID = 9934
+
+# Load MOMScript API
+$momapi = New-Object -comObject MOM.ScriptAPI
+
+trap
+{
+    $message = "`n $parameterString `n $($_.ToString())"
+    $momapi.LogScriptEvent($scriptName, $scriptEventID, 1, $message)
+    break
+}
+
+# Log script event that we are starting task
+if ( $debug )
+{
+    $message = "`nScript is starting. $parameterString"
+    $momapi.LogScriptEvent($scriptName, $scriptEventID, 0, $message)
+    Write-Debug -Message $message
+}
 
 $states = @{
 	5 = 'Assigned'
 	15 = 'Verified'
 	18 = 'Alert Storm'
 }
-  
+
 foreach ( $state in $states.GetEnumerator() )
 {
-	Add-SCOMResolutionState -Name $state.Value -ResolutionStateCode = $state.Key
+	$addScomResolutionStateParams = @{
+        Name = $state.Value
+        ResolutionStateCode = $state.Key
+    }
+
+    if ( $debug )
+    {
+        $addScomResolutionStateParamsString = ( $addScomResolutionStateParams.GetEnumerator() | ForEach-Object -Process { "`n$($_.Key) => $($_.Value)" } ) -join ''
+        $message = "Add-SCOMResolutionState $addScomResolutionStateParamsString"
+        $momapi.LogScriptEvent($scriptName, $scriptEventID, 0, $message)
+        Write-Debug -Message $message
+    }
+    Add-SCOMResolutionState @addScomResolutionStateParams
+}
+
+# Log an event for script ending and total execution time.
+$endTime = Get-Date
+$scriptTime = ( $endTime - $startTime ).TotalSeconds
+
+if ( $debug )
+{
+    $message = "`n Script Completed. `n Script Runtime: ($scriptTime) seconds."
+    $momapi.LogScriptEvent($scriptName, $scriptEventID, 0, $message)
+    Write-Debug -Message $message
 }
